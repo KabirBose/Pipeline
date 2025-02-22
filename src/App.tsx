6f2c68ba4ae8028a18878e0b0ab50492b511479d
Kabir Bose <kabirbose04@gmail.com>
2025-02-22 12:24:00 -0500

    updated aui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [title, setTitle] = useState<string | null>(null);
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/scrape");
+      const data = await response.json();
+      setTitle(data.title);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <>
-      <h1>Hello from React</h1>
-    </>
+    <div>
+      <h1>Pipeline</h1>
+      <h2>Fill out job applications in seconds!</h2>
+      <button onClick={handleClick}>Run</button>
+      {title && <h1>Scraped Title: {title}</h1>}
+    </div>
   );
-}
+};
 
 export default App;
